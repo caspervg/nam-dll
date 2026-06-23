fa94eccfa81eb12a785b3501be8a20771f525ade
Casper Van Gheluwe <caspervg@gmail.com>
2026-06-23 23:08:32 +0200

    fix: some fixes after merge

--- src/Patching.cpp
+++ src/Patching.cpp
@@ -114,20 +114,6 @@ void Patching::PatchTestEaxImmediate32(const uint32_t address, const uint32_t ex
 	PatchImmediate32(address + 1, expectedValue, newValue);
 }
 
-void Patching::OverwriteMemory(void* address, uint32_t newValue)
-{
-	DWORD oldProtect;
-	// Allow the executable memory to be written to.
-	THROW_IF_WIN32_BOOL_FALSE(VirtualProtect(
-		address,
-		sizeof(newValue),
-		PAGE_EXECUTE_READWRITE,
-		&oldProtect));
-
-	// Patch the memory at the specified address.
-	*((uint32_t*)address) = newValue;
-}
-
 void Patching::PatchImmediate16(const uint32_t address, const uint16_t expectedValue, const uint16_t newValue)
 {
 	auto* const immediate = reinterpret_cast<uint16_t*>(address);
@@ -147,41 +133,6 @@ void Patching::PatchImmediate16(const uint32_t address, const uint16_t expectedV
 
 	std::memcpy(immediate, &newValue, sizeof(newValue));
 	VirtualProtect(immediate, sizeof(newValue), oldProtect, &oldProtect);
-}
-
-void Patching::PatchImmediate32(const uint32_t address, const uint32_t expectedValue, const uint32_t newValue)
-{
-	auto* const immediate = reinterpret_cast<uint32_t*>(address);
-	if (*immediate == newValue)
-	{
-		return;
-	}
-
-	THROW_HR_IF(E_FAIL, *immediate != expectedValue);
-
-	DWORD oldProtect;
-	THROW_IF_WIN32_BOOL_FALSE(VirtualProtect(
-		immediate,
-		sizeof(newValue),
-		PAGE_EXECUTE_READWRITE,
-		&oldProtect));
-
-	std::memcpy(immediate, &newValue, sizeof(newValue));
-	VirtualProtect(immediate, sizeof(newValue), oldProtect, &oldProtect);
-}
-
-void Patching::PatchPushImmediate32(const uint32_t address, const uint32_t expectedValue, const uint32_t newValue)
-{
-	const uint8_t* const instruction = reinterpret_cast<uint8_t*>(address);
-	THROW_HR_IF(E_FAIL, instruction[0] != 0x68);
-	PatchImmediate32(address + 1, expectedValue, newValue);
-}
-
-void Patching::PatchTestEaxImmediate32(const uint32_t address, const uint32_t expectedValue, const uint32_t newValue)
-{
-	const uint8_t* const instruction = reinterpret_cast<uint8_t*>(address);
-	THROW_HR_IF(E_FAIL, instruction[0] != 0xa9);
-	PatchImmediate32(address + 1, expectedValue, newValue);
 }
 
 void Patching::PatchTestWordPtrEaxImmediate16(
